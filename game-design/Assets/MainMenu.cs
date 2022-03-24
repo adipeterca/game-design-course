dbe78d516b0cc1fb1e73cef7860ae4e3d2976a8e
Maria Ecaterina Olariu <mariaecaterina.olariu@gmail.com>
2022-03-24 18:29:49 +0200

    MainMenu Scene added

---
+++ game-design/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// buildIndex = 0 for MainMenu, buildIndex = 1 for MainScene ->
+    /// this can be found in File -> Build Settings
+    /// </summary>
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
