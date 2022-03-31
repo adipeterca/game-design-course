f9876f38967da47f191e915b14233868037b1127
adipeterca <adipeterca@gmail.com>
2022-03-31 14:43:01 +0300

    [FIX] Player controller rework to singleton

--- game-design/Assets/Scripts/PlayerModelController.cs
+++ game-design/Assets/Scripts/PlayerModelController.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Singleton class which handles the control and behaviour of the Player object.
+/// </summary>
 public class PlayerModelController : MonoBehaviour
 {
+    private PlayerModelController() { }
+
+    public static PlayerModelController Instance
+    {
+        get;
+        private set;
+    }
+
     // Multiplier for movement speed
     public float movementSpeedMultiplier = 1;
 
     // Rigidbody of the Player object
     private Rigidbody rb;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(Instance);
+        else
+            Instance = this;
+    }
 
     void Start()
     {
