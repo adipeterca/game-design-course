544782edd4fb8f678d1a52fe522f3a51e607fa0d
Theodor Vlad <vladtheodor0@gmail.com>
2022-04-28 15:09:23 +0300

    fixed bug where player could sprint while not holding down any key to move in a direction

--- game-design/Assets/Scripts/PlayerModelController.cs
+++ game-design/Assets/Scripts/PlayerModelController.cs
@@ -132,7 +132,11 @@ public class PlayerModelController : MonoBehaviour
 
     void UpdateStamina()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && GuiManagerController.Instance.GetStamina() > 0.0f)
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) &&
+            Input.GetKey(KeyCode.LeftShift) && GuiManagerController.Instance.GetStamina() > 0.0f)
         {
             sprinting = true;
             GuiManagerController.Instance.DecreaseStamina(staminaModifyValue);
